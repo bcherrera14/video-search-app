9abc4ccfc50cabf9e5723aca72a7c4deeda1cfdb
Bryan Herrera <bcherrera14@gmail.com>
2021-12-06 09:31:38 -0800

    restrict api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
-const KEY = 'AIzaSyDgd1xlzRT9E0b9RjpwSj9158hHaS_cTYk';
+import axios from 'axios';
+
+const KEY = 'AIzaSyCkgRoHBTU53u7_BrXh35HqAfcHwknfKp0';
+
+export default axios.create({
+	baseURL: 'https://www.googleapis.com/youtube/v3',
+	params: {
+		part: 'snippet',
+		maxResults: 5,
+		type: 'video',
+		key: KEY
+	}
+});
