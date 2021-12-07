0a692d9683f9df84806b60ac16d2cd02d23b3307
Bryan Herrera <bcherrera14@gmail.com>
2021-12-06 18:15:21 -0800

    add video detail embed

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,17 +1,24 @@
 import React from 'react';
 // import VideoItem from './VideoItem';
+import './VideoDetail.css';
 
 const VideoDetail = ({ video }) => {
 	if (!video) {
 		return <div>Loading...</div>;
 	}
+
+	const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
 	return (
-		<div className="card">
-			<div className="card-body">
-				<h4 className="card-title">{video.snippet.title}</h4>
-				<p className="card-text">{video.snippet.description}</p>
+		<div>
+			<div className="card mt-3">
+				<iframe class="embed-responsive-item" src={videoSrc} allowfullscreen />
+				<div className="card-body">
+					<h5 className="card-title">{video.snippet.title}</h5>
+					<p className="card-text">{video.snippet.description}</p>
+				</div>
+				<p />
 			</div>
-			<p />
 		</div>
 	);
 };
